67bba7f052fecb2bc6ff84826c216eaaa813ebc8
ThyVibeGamer <thebrogamer1234@gmail.com>
2025-11-02 10:25:22 -0700

    New Unity Sound Effects

--- Assets/Scripts/WormMove.cs
+++ Assets/Scripts/WormMove.cs
@@ -1,40 +1,72 @@
+// using UnityEngine;
+
+// public class WormMove : MonoBehaviour
+// {
+//     public float speed = 1.0f;
+//     public Vector3 pointA;
+//     public Vector3 pointB;
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
+//         pointA = transform.position;
+//         pointB = transform.position + new Vector3(1.4f, 0f, 0f);
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {   
+
+//         float time = Mathf.PingPong(Time.time * speed, 1);
+
+//         transform.position = Vector3.Lerp(pointA,  pointB, time);
+
+
+
+
+//         // if(transform.position.x < 0.7f)
+//         // {
+//         //     transform.rotation = Quaternion.Euler(0, 90, -90);
+//         //     transform.position += Vector3.right * Time.deltaTime; 
+//         // }
+
+//         // else if (transform.position.x > -0.7f)
+//         // {
+//         //     transform.rotation = Quaternion.Euler(180, 90, -90);
+//         //     transform.position -= Vector3.right * Time.deltaTime; 
+//         // }
+        
+
+//     }
+// }
+
 using UnityEngine;
 
 public class WormMove : MonoBehaviour
 {
     public float speed = 1.0f;
+    public float speedMultiplier = 1.1f; // How much to increase speed by each reset
     public Vector3 pointA;
     public Vector3 pointB;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private bool movingToB = true;
+
     void Start()
     {
         pointA = transform.position;
         pointB = transform.position + new Vector3(1.4f, 0f, 0f);
     }
 
-    // Update is called once per frame
     void Update()
-    {   
-
+    {
         float time = Mathf.PingPong(Time.time * speed, 1);
+        transform.position = Vector3.Lerp(pointA, pointB, time);
+    }
 
-        transform.position = Vector3.Lerp(pointA,  pointB, time);
-
-
-
-
-        // if(transform.position.x < 0.7f)as
-        // {
-        //     transform.rotation = Quaternion.Euler(0, 90, -90);
-        //     transform.position += Vector3.right * Time.deltaTime; 
-        // }
-
-        // else if (transform.position.x > -0.7f)
-        // {
-        //     transform.rotation = Quaternion.Euler(180, 90, -90);
-        //     transform.position -= Vector3.right * Time.deltaTime; 
-        // }
-        
-
+    // Called when clicked while inside detection zone
+    public void ResetToPointA()
+    {
+        transform.position = pointA;
+        speed *= speedMultiplier;
+        Debug.Log(name + " reset to pointA. New speed: " + speed);
     }
 }
