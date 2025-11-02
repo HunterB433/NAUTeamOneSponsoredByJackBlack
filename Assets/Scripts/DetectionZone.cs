50ad1f4d6de7316543bd33164f458230bf00c55a
ThyVibeGamer <thebrogamer1234@gmail.com>
2025-11-01 20:37:26 -0700

    Another worm update

--- Assets/Scripts/DetectionZone.cs
+++ Assets/Scripts/DetectionZone.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class DetectionZone : MonoBehaviour
 {
+<<<<<<< Updated upstream
     private void OnTriggerEnter(Collider other)
     {
         // This method is called when another collider enters the trigger zone.
@@ -14,10 +15,22 @@ public class DetectionZone : MonoBehaviour
             Debug.Log("Worm detected!");
             // Perform actions related to worm detection
         }
+=======
+    private bool wormInZone = false;
+    private Collider savedOther;
+    public WormMove wormMove;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name + " entered the detection zone!");
+        savedOther = other;
+        wormInZone = true;
+>>>>>>> Stashed changes
     }
 
     private void OnTriggerExit(Collider other)
     {
+<<<<<<< Updated upstream
         // This method is called when another collider leaves the trigger zone.
         Debug.Log(other.name + " left the detection zone!");
 
@@ -26,6 +39,19 @@ public class DetectionZone : MonoBehaviour
         {
             Debug.Log("Worm left the detection zone!");
             // Perform actions related to worm leaving
+=======
+        Debug.Log(other.name + " exited the detection zone!");
+        wormInZone = false;
+    }
+
+    private void Update()
+    {
+        if (wormInZone && Input.GetMouseButtonDown(0))
+        {
+            if (wormMove != null)
+                wormMove.speed = 0.0f;
+                Debug.Log(savedOther.name + " was clicked the detection zone!");
+>>>>>>> Stashed changes
         }
     }
 }
