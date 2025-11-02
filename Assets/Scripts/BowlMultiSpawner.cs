68e497993fef298b6819b18dbbab060bc18db677
bedog9 <wms72@nau.edu>
2025-11-02 01:05:00 -0700

    Finished Bowl Game
    Winning is showing in console missing win screen, timer and instructions

--- Assets/Scripts/BowlMultiSpawner.cs
+++ Assets/Scripts/BowlMultiSpawner.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 public class MultiSpawner : MonoBehaviour
 {
-    public GameObject objectType1; // prefab to spawn 15 times
+    public GameObject objectType1; // prefab to spawn 25 times
     public GameObject objectType2; // prefab to spawn 1 time
     public List<Transform> spawnPoints = new List<Transform>(); // 16 possible points
 
@@ -12,22 +12,35 @@ public class MultiSpawner : MonoBehaviour
         // copy spawn points list so we can remove used ones
         List<Transform> availablePoints = new List<Transform>(spawnPoints);
 
-        // ----- SPAWN 15 OBJECTS -----
+        // ----- SPAWN 25 OBJECTS -----
         for (int i = 0; i < 25; i++)
         {
             int index = Random.Range(0, availablePoints.Count);
             Transform chosenPoint = availablePoints[index];
 
-            Instantiate(objectType1, chosenPoint.position, chosenPoint.rotation);
+            // choose random upside-down rotation (180° on X or Z)
+            Quaternion upsideDownRotation;
+            if (Random.value < 0.5f)
+                upsideDownRotation = Quaternion.Euler(180f, chosenPoint.rotation.eulerAngles.y, chosenPoint.rotation.eulerAngles.z);
+            else
+                upsideDownRotation = Quaternion.Euler(chosenPoint.rotation.eulerAngles.x, chosenPoint.rotation.eulerAngles.y, 180f);
+
+            Instantiate(objectType1, chosenPoint.position, upsideDownRotation);
 
             availablePoints.RemoveAt(index); // remove to avoid duplicates
         }
 
-        // ----- SPAWN 16TH OBJECT -----
-        // pick randomly from remaining point
+        // ----- SPAWN FINAL OBJECT -----
         int lastIndex = Random.Range(0, availablePoints.Count);
         Transform lastPoint = availablePoints[lastIndex];
 
-        Instantiate(objectType2, lastPoint.position, lastPoint.rotation);
+        // same random upside-down rule for last object
+        Quaternion lastRotation;
+        if (Random.value < 0.5f)
+            lastRotation = Quaternion.Euler(180f, lastPoint.rotation.eulerAngles.y, lastPoint.rotation.eulerAngles.z);
+        else
+            lastRotation = Quaternion.Euler(lastPoint.rotation.eulerAngles.x, lastPoint.rotation.eulerAngles.y, 180f);
+
+        Instantiate(objectType2, lastPoint.position, lastRotation);
     }
 }
